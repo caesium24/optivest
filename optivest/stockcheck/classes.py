3020f786c6121d9365bf3fb67e1d56c60d176c4b
eshton.brogan <eshton.brogan@gmail.com>
2021-12-28 03:07:26 -0500

    added beginning of email class

--- optivest/stockcheck/classes.py
+++ optivest/stockcheck/classes.py
@@ -3,6 +3,8 @@ class Stock:
     self.ticker = ticker
     self.c_price = c_price
 
+class Gmail:
+  def __init__(self, imap_ssl_host, imap_ssl_port, username, password, server):
 class Session:
   def __init__(self, name, email, password, pin, mfa):
     self.name = name
