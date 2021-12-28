561f6e007b42174fede481a1b2e6b85d93b2b8f6
eshton.brogan <eshton.brogan@gmail.com>
2021-12-28 11:45:17 -0500

    Added path to __main__.py for classes file for improt

--- optivest/__main__.py
+++ optivest/__main__.py
@@ -1,4 +1,11 @@
-import quickstart
+import os
+import sys
+
+
+if os.path.exists(os.getcwd() + '\stockcheck') == True:
+  classes_path = os.path.join(os.getcwd() + '\stockcheck')
+  sys.path.append(classes_path)
+import classes
 
 def main():
 
--- optivest/stockcheck/classes.py
+++ optivest/stockcheck/classes.py
@@ -1,10 +1,8 @@
 class Stock:
-  def __init__(self, ticker, c_price,):
+  def __init__(self, ticker, c_price):
     self.ticker = ticker
     self.c_price = c_price
 
-class Gmail:
-  def __init__(self, imap_ssl_host, imap_ssl_port, username, password, server):
 class Session:
   def __init__(self, name, email, password, pin, mfa):
     self.name = name
