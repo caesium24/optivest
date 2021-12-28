65d3f538d1c23f016380d8e204f1ec47ccf89271
eshton.brogan <eshton.brogan@gmail.com>
2021-12-28 02:13:09 -0500

    Added stockcheck directory, added classes.py for stock class, added __init__.py, added __main__.py for execution script, added quickstart.py for setting up gmail access, add placeholder requirements to requirements.txt

--- optivest/stockcheck/classes.py
+++ optivest/stockcheck/classes.py
@@ -1,4 +1,13 @@
-
-
 class Stock:
   def __init__(self, name, ticker, c_price):
+    self.name = name
+    self.ticker = ticker
+    self.c_price = c_price
+
+class Session:
+  def __init__(self, name, email, password, pin, mfa):
+    self.name = name
+    self.email = email
+    self.password = password
+    self.pin = pin
+    self.mfa = mfa
